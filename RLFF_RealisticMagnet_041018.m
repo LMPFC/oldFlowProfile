dd7fb6fb38eac946ab44afb37239fb6d1e46cb8b
LMPFC <afisher@sunfire04.pppl.gov>
2018-10-18 16:27:47 -0400

    another test commit

--- RLFF_RealisticMagnet_041018.m
+++ RLFF_RealisticMagnet_041018.m
@@ -8,7 +8,7 @@ close all
 pm = 10;                    % Magic magnet number
 mu0 = 4*pi*1e-7;            % Permittivity of free space
 
-
+% Test to see if cluster saved github info
 %% Magnetic field generation
 % radialRes = 400;
 % lengthRes = 800;
